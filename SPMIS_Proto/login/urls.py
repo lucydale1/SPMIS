cf988cdc0dae8a439a8634c417067816144f32ef
Josh-McColl <McCollJ@cardiff.ac.uk>
2017-03-19 00:05:40 +0000

    changed home url, update saved paper link name

--- SPMIS_Proto/login/urls.py
+++ SPMIS_Proto/login/urls.py
@@ -7,7 +7,7 @@ from login.views import register1, registration_complete, results, savedPapers
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^home/$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
     #url(r'^results/$',  TemplateView.as_view(template_name='results.html'), name='results'),
