80c264f738d59c6dc4f3522ae0e17018ffdf48c1
jakeelliot1 <jakeelliotmorgan@gmail.com>
2017-05-04 12:48:29 +0100

    debug is false

--- SPMIS_Proto/login/settings.py
+++ SPMIS_Proto/login/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0^q9&^oblkc28287hb-2ub3pnyo9nub@-@=m56)g*mn_u8m@14'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
